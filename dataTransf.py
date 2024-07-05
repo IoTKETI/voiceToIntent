84e4db491838ab35813b0a468427ee7d16bc9fdb
IoTKETI <iot.keti@gmail.com>
2024-07-05 20:11:10 +0900

    convert output to 0.92 intent style

---
+++ dataTransf.py
@@ -0,0 +1,91 @@
+import uuid
+import datetime
+
+def transform_data(input_data):
+    # in case of "stand"
+    if input_data["intents"][0]["name"] == "stand":
+        transformed_data = {
+            "eventId": str(uuid.uuid4()),
+            "eventTime": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S,%f')[:-3] + '+09:00',
+            "eventIntents": [
+                {
+                    "text": input_data["text"],
+                    "intents": [
+                        {
+                            "id": "intent$000000000000008",
+                            "name": input_data["intents"][0]["name"],
+                            "confidence": f"{input_data["intents"][0]["confidence"]:.4f}"
+                        }
+                    ],
+                    "entities": {
+                        "deviceName": [
+                            {
+                                "id": "entity$00000000001",
+                                "name": "deviceName",
+                                "value": input_data["entities"]["deviceName:deviceName"][0]["value"],
+                                "confidence": f"{input_data["entities"]["deviceName:deviceName"][0]["confidence"]:.4f}"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+        return transformed_data
+    # in case of "sit"
+    elif input_data["intents"][0]["name"] == "sit":
+        transformed_data = {
+            "eventId": str(uuid.uuid4()),
+            "eventTime": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S,%f')[:-3] + '+09:00',
+            "eventIntents": [
+                {
+                    "text": input_data["text"],
+                    "intents": [
+                        {
+                            "id": "intent$000000000000009",
+                            "name": input_data["intents"][0]["name"],
+                            "confidence": f"{input_data["intents"][0]["confidence"]:.4f}"
+                        }
+                    ],
+                    "entities": {
+                        "deviceName": [
+                            {
+                                "id": "entity$00000000001",
+                                "name": "deviceName",
+                                "value": input_data["entities"]["deviceName:deviceName"][0]["value"],
+                                "confidence": f"{input_data["entities"]["deviceName:deviceName"][0]["confidence"]:.4f}"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+        return transformed_data
+    # in case of "ready"
+    elif input_data["intents"][0]["name"] == "ready":
+        transformed_data = {
+            "eventId": str(uuid.uuid4()),
+            "eventTime": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S,%f')[:-3] + '+09:00',
+            "eventIntents": [
+                {
+                    "text": input_data["text"],
+                    "intents": [
+                        {
+                            "id": "intent$000000000000010",
+                            "name": input_data["intents"][0]["name"],
+                            "confidence": f"{input_data["intents"][0]["confidence"]:.4f}"
+                        }
+                    ],
+                    "entities": {
+                        "deviceName": [
+                            {
+                                "id": "entity$00000000001",
+                                "name": "deviceName",
+                                "value": input_data["entities"]["deviceName:deviceName"][0]["value"],
+                                "confidence": f"{input_data["entities"]["deviceName:deviceName"][0]["confidence"]:.4f}"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+        return transformed_data
